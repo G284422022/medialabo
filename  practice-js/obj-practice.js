37a7a9d9229c5714c12d7bd5a935fefb5e49728f
G2284422022 <228442@st.takushoku-u.ac.jp>
2023-05-11 17:27:40 +0900

    kazuate

---  practice-js/obj-practice.js
+++  practice-js/obj-practice.js
@@ -16,3 +16,13 @@ let gakka = [
 
 //////////////// ここから下にプログラムを書きたそう!
 
+console.log(campus.address);
+for (let n of campus.buildingD){
+	console.log(n);
+}
+if (gakka.name !== undefined) {
+  }
+for (let y of gakka){
+	console.log(y.name);
+
+}
