1e6d0a71451abf8d47e80ab91dce83dc1183e10c
G2284422022 <228442@st.takushoku-u.ac.jp>
2023-05-25 22:00:57 +0900

    5-2

--- practice-event/event-intro.js
+++ practice-event/event-intro.js
@@ -5,7 +5,8 @@ let b1 = document.querySelector('button#blue');
 b1.addEventListener('click', changeH1ColorRed);
 
 //// 以下，練習5-2 でコードを追加
-
+let b2 = document.querySelector('button#random');
+b2.addEventListener('click',changeBackgroundColorRandom );
 
 ////////////////////////////////////////////////////////
 // 2. イベントハンドラ（イベントが発生したら実行する関数）
@@ -19,6 +20,7 @@ function changeH1ColorRed() {
 }
 
 function changeBackgroundColorRandom() {
+
 	// ランダムな RGB の色
 	let r = Math.floor(Math.random() * 256);
 	let g = Math.floor(Math.random() * 256);
